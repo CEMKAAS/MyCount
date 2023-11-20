47d9cdf93fc8012978c784b8a9542d7ee3e63260
CEMKAAS-Laptop <zaroslikob@mail.ru>
2023-11-20 16:42:38 +0300

    bottom

--- app/src/main/java/com/zaroslikov/mycountjava2/MainActivity.java
+++ app/src/main/java/com/zaroslikov/mycountjava2/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.zaroslikov.mycountjava2.db.MyDataBaseHelper;
 
 import java.util.Calendar;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton plus, minus;
     private int countPush;
     private int stepPush;
+    private BottomSheetDialog bottomSheetDialog;
+    private String appBarTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,20 +35,60 @@ public class MainActivity extends AppCompatActivity {
         plus = findViewById(R.id.button_plus);
         minus = findViewById(R.id.button_minus);
 
+
+
         Cursor cursor = myDB.lastReadProject();
 
         if (cursor == null && cursor.getCount() == 0){
             Calendar calendar = Calendar.getInstance();
             String time = (calendar.get(Calendar.DAY_OF_MONTH)) + "." + (calendar.get(Calendar.MONTH) + 1)+ "." + (calendar.get(Calendar.YEAR));
             myDB.insertToDb("Мой счет", 0,1,0, time);
+            appBarTitle = "Мой счет";
             countPush = 0;
             stepPush = 1;
         }else{
             cursor.moveToNext();
+            appBarTitle = cursor.getString(0);
             countPush = cursor.getInt(1);
             stepPush = cursor.getInt(2);
-
         }
+
+        MaterialToolbar appBar = findViewById(R.id.topAppBar);
+        appBar.setTitle(appBarTitle);
+
+        appBar.setNavigationIcon(R.drawable.baseline_format_list_bulleted_24);
+        appBar.setOnMenuItemClickListener(item -> {
+            int position = item.getItemId();
+            if (position == R.id.refresg) {
+                bottomSheetDialog.show();
+            } else if (position == R.id.setting) {
+                bottomSheetDialog.show();
+            }
+            return true;
+        });
+        appBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.show();
+            }
+        });
+
+        //Добавляем bottobSheet
+        public void showBottomSheetDialog() {
+
+            bottomSheetDialog = new BottomSheetDialog(this);
+            bottomSheetDialog.setContentView(R.layout.fragment_bottom);
+
+//            animalsSpinerSheet = bottomSheetDialog.findViewById(R.id.menu);
+//            categorySpinerSheet = bottomSheetDialog.findViewById(R.id.menu2);
+//
+//            animalsSpinerSheet.stVisibility(View.GONE);
+//            categorySpinerSheet.setVisibility(View.GONE);
+//
+//            dataSheet = bottomSheetDialog.findViewById(R.id.data_sheet);
+//            buttonSheet = bottomSheetDialog.findViewById(R.id.button_sheet);
+        }
+
 
 
         plus.setOnClickListener(new View.OnClickListener() {
